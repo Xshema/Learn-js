43c236d06073b057e77458f7406bb64481bb9ca7
Serge <shemaserge194@gmail.com>
2026-06-18 17:31:44 +0200

    testing 🫣

--- DOM/script.js
+++ DOM/script.js
@@ -1,26 +1,4 @@
-let count = 0
-
-const title = document.getElementById("title")
-
-const increment = document.getElementById("increment")
-
-const decrement = document.getElementById("decrement")
-
-function update(){
-    title.textContent = count
-}
-
-
-increment.addEventListener('click', ()=>{
-    count++
-     update()
-})
-
-
-decrement.addEventListener('click', ()=>{
-    count--
-    update()
-})
+console.log("print");
 
 
 
